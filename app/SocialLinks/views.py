44879e44475e524700770ee497c7cf50ef493d1e
Chris Schmitt <chrisschmitt58@yahoo.com>
2021-11-16 18:15:16 -0600

    Fixed so the index.html and css pages render correctly. Fixed static and templates directories

--- app/SocialLinks/views.py
+++ app/SocialLinks/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-        template = loader.get_template("SocialLinks/index.html")
-        return HttpResponse(template.render)
+        return render(request, "SocialLinks/index.html")
