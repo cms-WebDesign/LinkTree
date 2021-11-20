b419cd38a399580bc49f8d8dfc31e02afad0c42b
Chris Schmitt <chrisschmitt58@yahoo.com>
2021-11-20 13:34:54 -0600

    added ability to pass style variables from views.py to the index.html template

--- app/SocialLinks/views.py
+++ app/SocialLinks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Create your views here.
+
 def index(request):
-        return render(request, "SocialLinks/index.html")
+    linkcolor = "#000"
+    font = "Brush Script MT"
+    return render(request, "SocialLinks/index.html", {"linkcolor":linkcolor, "font":font})
